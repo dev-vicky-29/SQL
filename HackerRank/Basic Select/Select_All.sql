1e1005b334ce310667de13808c742d1369045608
dev-vicky-29 <150275960+dev-vicky-29@users.noreply.github.com>
2024-12-13 16:47:52 +0530

    Create Select_All.sql

---
+++ HackerRank/Basic Select/Select_All.sql
@@ -0,0 +1,22 @@
+/*
+
+Que: Query all columns for every row in the CITY table.
+
+## Input Format
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+
+
+*/
+
+
+SELECT * From CITY;
